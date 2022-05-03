cbe5f0aa844ae9521c283d9446b1c4bb7bd52d4d
ASKK <102436728+ASKiere@users.noreply.github.com>
2022-05-03 08:29:11 -0400

    aaa

--- center .js
+++ center .js
@@ -1,4 +1,4 @@
           console.log("sub to mick! also, hi mom")
-          var cont1 = getElementById("cont") 
+          const cont1 = getElementById("cont") 
           function qwert() = {  cent3: center(cont1) }
-          var cent1 = qwert.cent3
+          const cent1 = qwert.cent3
